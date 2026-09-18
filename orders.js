7a06773f8eee1752942cbe116c3f09e282ecb10e
LAB11_USER ON WS22 <L11X09W22@LabHCI.net>
2026-09-18 08:57:21 +0800

    Add VIP bonus loyalty points rule - Maquiling.Justin

--- orders.js
+++ orders.js
@@ -16,7 +16,13 @@ function cancelOrder(order) {
 }
 
 function calculateLoyaltyPoints(order) {
-  return Math.floor(order.total / 10);
+    const basePoints  = Math.floor(order.total / 10);
+    if(order.total > 100){
+      return  Math.floor(basePoints * 1.5);
+    }else{
+     return basePoints;
+    }
+
 }
 
 module.exports = { createOrder, applyDiscount, cancelOrder, calculateLoyaltyPoints };
